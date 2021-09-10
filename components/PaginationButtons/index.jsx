be0e75b38c847769bbba31ecd24f9ca2d23670da
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-10 13:36:39 +0700

    :triangular_flag_on_post: done building the pagination functionalities
    checked

--- components/PaginationButtons/index.jsx
+++ components/PaginationButtons/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export const PaginationButtons = () => {
+  const router = useRouter();
+
+  const startIndex = Number(router.query.start) || 0;
+
   return (
-    <div>
-      <h1>booo</h1>
+    <div className="flex justify-between text-blue-700 mb-10 max-w-lg">
+      {startIndex >= 10 && (
+        <Link
+          href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
+          passHref
+        >
+          <div className="flex items-center flex-grow flex-col cursor-pointer hover:underline">
+            <ChevronLeftIcon className="h-5" />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+
+      <Link
+        href={`/search?term=${router.query.term}&start=${startIndex + 10}`}
+        passHref
+      >
+        <div className="flex items-center flex-grow flex-col cursor-pointer hover:underline">
+          <ChevronRightIcon className="h-5" />
+          <p>Next</p>
+        </div>
+      </Link>
     </div>
   );
 };
